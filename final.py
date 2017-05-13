ee594bd9e4b7944c5b91750f040c4b53bd0d852c
Joey <joey@joeys-mbp.middlebury.edu>
2017-05-13 17:42:12 -0400

    backtrack preds

--- final.py
+++ final.py
@@ -4,9 +4,11 @@ import argparse
 import csv 
 
 
-REF = "TTGTTTTCTTTTGAAACAGAATCTCACTCTGCAGTCCAGGCTGGAGTGCAGCGGTGCAATCTTGGCTCACTGCAACCTCTGCCTTGTGAGTTCAAGCGATTCTCCTGCCTCAGCCTCTAGACTAGCTGGGATTACAGGTGCATGCCACCATGTCCAGCTAACTTTTTTTGTTTGTTTATTTGTTTGTTTGTTTGTTTTGAGACGGAGTCTTGCTCTATTGCCCAGGCTGGAGTGCAGTGGTGCAATCTCGGCTCACTGCAAGCTTCACCTCCCGGGTTCATGCCATTCTTCTGCCTCAGCCTCCCAAGTAGCTGGGACTACAAGTGCCCGCCACCA"
+# REF = "TTGTTTTCTTTTGAAACAGAATCTCACTCTGCAGTCCAGGCTGGAGTGCAGCGGTGCAATCTTGGCTCACTGCAACCTCTGCCTTGTGAGTTCAAGCGATTCTCCTGCCTCAGCCTCTAGACTAGCTGGGATTACAGGTGCATGCCACCATGTCCAGCTAACTTTTTTTGTTTGTTTATTTGTTTGTTTGTTTGTTTTGAGACGGAGTCTTGCTCTATTGCCCAGGCTGGAGTGCAGTGGTGCAATCTCGGCTCACTGCAAGCTTCACCTCCCGGGTTCATGCCATTCTTCTGCCTCAGCCTCCCAAGTAGCTGGGACTACAAGTGCCCGCCACCA"
+REF = "PENALTY"
 START = 1520505
 END = 1520840
+PENALTY = -5
 
 def read_file(filename,delim=","):
     with open(filename, 'rb') as csvfile:
@@ -16,9 +18,9 @@ def read_file(filename,delim=","):
             variants.append(row)
         return variants
 
-def ref_to_graph():
+def ref_to_graph(ref):
     graph = list()
-    for i in range(len(REF)):
+    for i in range(len(ref)):
         base = REF[i]
         graph.append({
             'base': base,
@@ -83,9 +85,9 @@ def graph_to_dict(graph):
 
     return dic_graph
 
-graph = ref_to_graph()
-graph_varied = variants_onto_graph(graph,variants)
-graph_dict = graph_to_dict(graph_varied)
+graph = ref_to_graph(REF)
+# graph_varied = variants_onto_graph(graph,variants)
+graph_dict = graph_to_dict(graph)
 
 L = []
 marked = []
@@ -107,32 +109,77 @@ while len(toVisit) > 0:
     visit(toVisit[0])
 
 def fit(graph, read, penalty, graph_dict):
-    matrix = [[0 for x in range(len(graph)+1)] for y in range(len(read)+1)]
+    matrix = [[(0,(-1,-1)) for x in range(len(graph)+1)] for y in range(len(read)+1)]
 
     for i in range(0,len(read)+1):
-        matrix[i][0] = 0
+        matrix[i][0] = (0,(-1,-1))
     for j in range(0,len(graph)+1):
-        matrix[0][j] = 0
+        matrix[0][j] = (0,(-1,-1))
         
     for i in range(1, len(read)+1):
         for j in range(1, len(graph)+1):
             #score
             scores = []
             for in_edge in graph_dict[graph[j-1]]["in"]:
-            	if in_edge == -1:
-            		continue
-            	#pams
-            	a = (read[i-1], graph_dict[in_edge]["base"])
-            	if a not in pam250:
-                	a = (graph_dict[in_edge]["base"], read[i-1])
+                if in_edge == -1:
+                    continue
+                #pams
+                a = (read[i-1], graph_dict[in_edge]["base"])
+                if a not in pam250:
+                    a = (graph_dict[in_edge]["base"], read[i-1])
 
-                scores.append(matrix[i-1][in_edge] + pam250[a])
-                scores.append(matrix[i][in_edge] + penalty)
+                scores.append( (matrix[i-1][graph.index(in_edge)][0] + pam250[a], (i-1,graph.index(in_edge)) ))
+                scores.append((matrix[i][graph.index(in_edge)][0] + penalty, (i,graph.index(in_edge)) ) )
 
-            scores.extend([matrix[i-1][j], 0])
+            scores.extend([ (matrix[i-1][j][0] + penalty, (i-1,j)) , (0,(-1,-1))])
                      
-            matrix[i][j] = max(scores)
+            matrix[i][j] = max(scores,key=lambda p: p[0])
 
     return matrix
 
-print(fit(L, 'GTGCAATCTTGGCTCACTGCAACCTCTGCCTTGTGAGTTCAAGCGATTCTCCTGCCTCAGCCTCTAGACTAGCTGGGATTACAGGTGCATGCCACCATGT', -5, graph_dict))
+def backtrack(scores, max_score, penalty, topo, read, graph_dict):
+    score = max_score
+    max_i = [max(j,key=lambda p: p[0]) for j in scores].index(max_score)
+    max_j = scores[max_i].index(max_score)
+    print(max_i,max_j)
+    # print([i[:max_j] for i in scores[:max_i]])
+    i, j = max_i, max_j
+    s1, s2 = '', ''
+    # print(scores)
+
+    # print([l[max_j-10:max_j] for l in scores[max_i-10:max_i] ])
+    
+    while (i > 0 and j > 0):
+        cur = scores[i][j]
+        if cur == 0:
+            break
+
+        pred = cur[1]
+        if pred[0] == i:
+            # came from same column
+            s2 = "-"+s2     
+            s1 = str(graph_dict[topo[j-1]]['base']) + s1
+        elif pred[1] == j:
+            # came from same row
+            s2 = read[i-1]+s2     
+            s1 = "-" + s1
+        else:
+            # use both
+            s2 = read[i-1]+s2
+            s1 = str(graph_dict[topo[j-1]]['base']) + s1
+        
+        i,j = pred
+    return s1, s2
+            
+reads = [
+    {"RAW":'MEANLY'},
+    {"RAW":'ENALTY'}
+    ]
+for read in reads:
+    mat = fit(L, read['RAW'], PENALTY, graph_dict)
+    for row in mat:
+        print(row)
+    topo,align = backtrack(mat,max([max(l,key=lambda p:p[0]) for l in mat]), PENALTY, L, read['RAW'], graph_dict)
+    print(topo)
+    print(align)
+    print('∆'*10)
